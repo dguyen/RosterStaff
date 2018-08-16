ba0d1baaa2a2f889652337f4873c8f6b5a05f25f
bailey <bjin6@student.monash.edu>
2018-08-17 01:46:09 +1000

    Added staff listView with dummy data

--- src/app/staff/shifts/shift-view/list/list.component.ts
+++ src/app/staff/shifts/shift-view/list/list.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+const Job_list: AvailableJobs[] = [
+  {Location: 'Mount Waverley', Start: 1400, End: 1600, Duration: 200, Note: 'n/a', Replacement: 'n/a', On_Duty: 'yeet'},
+  {Location: 'Glen Waverley', Start: 1600, End: 2200, Duration: 600, Note: 'n/a', Replacement: 'n/a', On_Duty: 'yeet'},
+  {Location: 'Ashwood', Start: 1500, End: 2000, Duration: 500, Note: 'n/a', Replacement: 'n/a', On_Duty: 'yeet'},
+  {Location: 'Clayton', Start: 1400, End: 2000, Duration: 600, Note: 'n/a', Replacement: 'n/a', On_Duty: 'yeet'},
+  {Location: 'Slamtown Downtown', Start: 2000, End: 2200, Duration: 200, Note: 'n/a', Replacement: 'n/a', On_Duty: 'yeet'},
+
+
+];
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,9 +17,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  displayedColumns: string[] = ['Location', 'Start', 'End', 'Duration', 'Note', 'Replacement', 'On_Duty'];
+  columnsToDisplay: string[] = this.displayedColumns;
+  dataSource = Job_list;
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
